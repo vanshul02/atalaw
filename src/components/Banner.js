e646aa32c2d58b7623d608d9cb6438e67dba73c5
Vanshul Ahuja <vanshul.ahuja@gmail.com>
2025-06-13 22:39:11 +0530

    Fixed nested p console error

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -15,13 +15,13 @@ const Banner = () => {
             lg:mt-[70px] leading-tight lg:leading-[90px]'>
             WELCOME TO<br /> <span className='text-accent'>ATA Law Partners & Associates</span>
           </h1>
-          <p className='max-w-5xl text-xl mx-auto lg:mb-[50px] text-accent
+          <div className='max-w-5xl text-xl mx-auto lg:mb-[50px] text-accent
               lg:max-0 '>
             <p className='text-2xl'>Strategic Legal Advisory | Expert Representation | Specializing in IPR and Corporate Law <br /></p>
             <br /><br />
             ATA Law Partners & Associates delivers comprehensive legal solutions, providing strategic advisory services and expert representation in Intellectual Property Rights (IPR) and Corporate Law matters.
             We are your trusted partners, committed to safeguarding your legal rights and advancing your business interests with precision and integrity.
-          </p>
+          </div>
         </div>
       </div>
     </section>
